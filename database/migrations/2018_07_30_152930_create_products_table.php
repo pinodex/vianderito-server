14631ef44f6ce615179d094e9e0921e18c1d5986
Raphael Marco <raphaelmarco@outlook.com>
2018-08-02 17:57:50 +0800

    Add category to product

--- database/migrations/2018_07_30_152930_create_products_table.php
+++ database/migrations/2018_07_30_152930_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('manufacturer_id');
+            $table->uuid('category_id');
             $table->char('upc', 12);
             $table->string('name');
             $table->text('description');
