edb78d430ada5aed60f8e48bbba41db40030bd6c
Raphael Marco <raphaelmarco@outlook.com>
2018-09-02 23:05:20 +0800

    Reword account new password email content

--- resources/views/emails/account_new_password_set.blade.php
+++ resources/views/emails/account_new_password_set.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 # Hi {{ $name }},
 
-The password to your Vianderito employee account was changed.
+This is to notify you that your Vianderito employee account password was changed.
 
 For security, this action was received from a {{ $device_name }} with IP address {{ $ip_address }} on {{ $timestamp }}.
 
-If you didn't trigger this action, we recommend you immediately review your account security, including changing your password. If you determine that this activity is malicious please contact your administrator.
+If you didn't made a password change, we recommend you immediately review your account security, including changing your password. If you determine that this activity is malicious please contact your administrator.
 
 @endcomponent
