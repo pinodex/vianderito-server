9301db0b4d9e49ba9302163366c174c745f0d6a4
Raphael Marco <raphaelmarco@outlook.com>
2018-09-23 00:58:07 +0800

    Add log messages for restore and destroy model function

--- resources/lang/en/action.php
+++ resources/lang/en/action.php
@@ -12,35 +12,51 @@ return [
     'accounts:reset_password' => 'Reset password for account :name',
     'accounts:enable' => 'Enable account :name',
     'accounts:disable' => 'Disable account :name',
+    'accounts:restore' => 'Restore account :name',
+    'accounts:destroy' => 'Permanently deleted account :name',
 
     'departments:create' => 'Created department :name',
     'departments:edit' => 'Edited department :name',
     'departments:set_permissions' => 'Set department permissions for :name',
     'departments:delete' => 'Deleted department :name',
+    'departments:restore' => 'Restore department :name',
+    'departments:destroy' => 'Permanently deleted department :name',
 
     'categories:create' => 'Created category :name',
     'categories:edit' => 'Edited category :name',
     'categories:delete' => 'Deleted category :name',
+    'categories:restore' => 'Restore category :name',
+    'categories:destroy' => 'Permanently deleted category :name',
 
     'suppliers:create' => 'Created supplier :name',
     'suppliers:edit' => 'Edited supplier :name',
     'suppliers:delete' => 'Deleted supplier :name',
+    'suppliers:restore' => 'Restore supplier :name',
+    'suppliers:destroy' => 'Permanently deleted supplier :name',
 
     'products:create' => 'Created product :name',
     'products:edit' => 'Edited product :name',
     'products:set_picture' => 'Set product picture for :name',
     'products:delete' => 'Deleted product :name',
+    'products:restore' => 'Restore product :name',
+    'products:destroy' => 'Permanently deleted product :name',
 
     'inventories:create' => 'Created inventory for :product',
     'inventories:edit' => 'Edited inventory for :product',
     'inventories:delete' => 'Deleted inventory :id for :product',
+    'inventories:restore' => 'Restore inventory :name',
+    'inventories:destroy' => 'Permanently deleted inventory :name',
 
     'users:create' => 'Created user :name',
     'users:edit' => 'Edited user :name',
     'users:set_avatar' => 'Set user avatar for :name',
     'users:delete' => 'Deleted user :name',
+    'users:restore' => 'Restore user :name',
+    'users:destroy' => 'Permanently deleted user :name',
 
     'coupons:create' => 'Created coupon code :code',
     'coupons:edit' => 'Edited coupon code :code',
-    'coupons:delete' => 'Deleted coupon code :code'
+    'coupons:delete' => 'Deleted coupon code :code',
+    'coupons:restore' => 'Restore coupon :name',
+    'coupons:destroy' => 'Permanently deleted coupon :name',
 ];
