aa1804e83412acb840b52d3801162835345004d7
Raphael Marco <raphaelmarco@outlook.com>
2018-09-27 09:10:28 +0800

    Add dummy users in seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,9 +13,26 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Sample User',
-            'username' => 'user',
+            'name' => 'John Doe',
+            'username' => 'johndoe',
             'password' => 'password',
+            'phone_number' => '09551234567',
+            'is_verified' => true
+        ]);
+
+        User::create([
+            'name' => 'Juan Dela Cruz',
+            'username' => 'juandelacruz',
+            'password' => 'password',
+            'phone_number' => '09995551111',
+            'is_verified' => true
+        ]);
+
+        User::create([
+            'name' => 'Jane Smith',
+            'username' => 'janesmith',
+            'password' => 'password',
+            'phone_number' => '09759994545',
             'is_verified' => true
         ]);
     }
